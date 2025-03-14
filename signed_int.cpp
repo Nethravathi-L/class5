2f63bad8ed382fc6c5990a14b1cd7d0497dd8473
netra <nethra81122@gmail.com>
2025-03-14 19:08:24 +0530

    Adding signed_int keyword

---
+++ signed_int.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    signed int a = -5;  
+    signed int b = 10;  
+
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+
+    signed int result = a + b; 
+    cout << "a + b = " << result << endl;
+
+    return 0;
+}
